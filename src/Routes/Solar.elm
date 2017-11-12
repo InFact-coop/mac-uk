be5bceeeec028d6afeeece9146b8fef3fa8a7812
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-11-12 20:07:41 +0000

    #15 #11 #3 #9 created function to take the title and organisation description and put it on the right side of the ws, as a consequence,  the text does not rotate anymore as it is in a fixed position

--- src/Routes/Solar.elm
+++ src/Routes/Solar.elm
@@ -8,128 +8,109 @@ import Types exposing (..)
 
 solar : Model -> Html Msg
 solar model =
-    section [ class "clearfix" ]
-        [ ul [ class "solarsystem" ]
-            [ li [ class "sun" ]
-                [ a [ href "#sun" ]
-                    [ span []
-                        []
+    let
+        ( title, description ) =
+            getOrganisationTitleAndDescription model.focusedOrganisation
+    in
+        section [ class "clearfix" ]
+            [ div [ class "ma3 fr" ]
+                [ h4 [ class "f3 white pa2" ] [ text title ]
+                , p [ class "f5 white pa2" ]
+                    [ text description
+                    , a [ href "#organigram" ]
+                        [ img [ src "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-info-6.png&r=255&g=255&b=255", class "mw2" ] [] ]
                     ]
                 ]
-            , li [ classes [ "mercury", getPlanetBlurClass model PeersFamily ] ]
-                [ a [ href "#mercury" ]
-                    [ span []
-                        []
-                    ]
-                ]
-            , li [ classes [ "venus", "border", getPlanetBlurClass model PeersFamily ] ]
-                [ a [ href "#venus" ]
-                    [ span []
-                        []
-                    ]
-                ]
-            , li [ classes [ "earth", getPlanetBlurClass model SchoolsCommunity ], onClick (FocusOrganisation ChildrenServices) ]
-                [ span []
-                    [ span [ class "moon" ]
-                        []
-                    ]
-                , div []
-                    [ div []
-                        (if model.focusedOrganisation == Just ChildrenServices then
-                            [ h4 [ class "f4" ] [ text "Barnet Children and Young People Plan" ]
-                            , p [ class "f5" ] [ text "A partnership with children, young people and different organisations that has produced a new child-friendly plan for 2016 - 2020 that reflects the priorities, needs and aspirations of the local population." ]
-                            , a [ href "#organigram" ] [ img [ src "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2012/png/iconmonstr-info-6.png&r=255&g=255&b=255", class "mw2" ] [] ]
-                            ]
-                         else
+            , ul [ class "solarsystem" ]
+                [ li [ class "sun" ]
+                    [ a [ href "#sun" ]
+                        [ span []
                             []
-                        )
-                    ]
-                ]
-            , li
-                [ classes
-                    [ "mars"
-                    , getPlanetBlurClass model SchoolsCommunity
-                    ]
-                ]
-                [ span []
-                    []
-                ]
-            , li
-                [ classes
-                    [ "asteroids_meteorids"
-                    , getPlanetBlurClass model SchoolsCommunity
-                    ]
-                ]
-                [ span []
-                    []
-                ]
-            , li
-                [ classes
-                    [ "jupiter"
-                    , "border"
-                    , getPlanetBlurClass model HealthHousingEmployabilitySafety
-                    ]
-                , onClick (FocusOrganisation SocialCareDirect)
-                ]
-                [ span [ class (getPlanetBlurClass model HealthHousingEmployabilitySafety) ]
-                    []
-                ]
-            , div []
-                [ div []
-                    (if model.focusedOrganisation == Just SocialCareDirect then
-                        [ h4 [ class "f4" ] [ text "Social Care Direct" ]
-                        , p [ class "f5" ] [ text "For adults with social care needs and their carers, advice and information about adult social care services and safeguarding alerts" ]
                         ]
-                     else
-                        []
-                    )
-                ]
-            , li [ classes [ "saturn", getPlanetBlurClass model HealthHousingEmployabilitySafety ], onClick (FocusOrganisation REACHteam) ]
-                [ span []
-                    [ span [ class "ring" ]
-                        [ text "" ]
                     ]
-                , div []
-                    [ div []
-                        (if model.focusedOrganisation == Just REACHteam then
-                            [ h4 [ class "f4" ] [ text "REACH team" ]
-                            , p [ class "f5" ] [ text " a multi-professional and multi-disciplinary team with expertise in safeguarding, mental health, parenting, education and learning, health and communication to jointly assess, plan and intervene with young people presenting with high risk/high vulnerability" ]
-                            ]
-                         else
+                , li [ classes [ "mercury", getPlanetBlurClass model PeersFamily ] ]
+                    [ a [ href "#mercury" ]
+                        [ span []
                             []
-                        )
+                        ]
                     ]
-                ]
-            , li [ classes [ "uranus", "border", getPlanetBlurClass model CulturePolitics ] ]
-                [ a [ href "#uranus" ]
-                    [ span [ classes [ "1", getPlanetBlurClass model CulturePolitics ] ]
-                        []
-                    , span [ classes [ "2", getPlanetBlurClass model CulturePolitics ] ]
-                        []
+                , li [ classes [ "venus", "border", getPlanetBlurClass model PeersFamily ] ]
+                    [ a [ href "#venus" ]
+                        [ span []
+                            []
+                        ]
                     ]
-                ]
-            , li [ classes [ "neptune", getPlanetBlurClass model CulturePolitics ] ]
-                [ a [ href "#neptune" ]
+                , li [ classes [ "earth", getPlanetBlurClass model SchoolsCommunity ], onClick (FocusOrganisation ChildrenServices) ]
+                    [ span []
+                        [ span [ class "moon" ]
+                            []
+                        ]
+                    ]
+                , li
+                    [ classes
+                        [ "mars"
+                        , getPlanetBlurClass model SchoolsCommunity
+                        ]
+                    ]
                     [ span []
                         []
                     ]
-                ]
-            , li [ classes [ "pluto", getPlanetBlurClass model CulturePolitics ] ]
-                [ a [ href "#pluto" ]
+                , li
+                    [ classes
+                        [ "asteroids_meteorids"
+                        , getPlanetBlurClass model SchoolsCommunity
+                        ]
+                    ]
                     [ span []
                         []
                     ]
+                , li
+                    [ classes
+                        [ "jupiter"
+                        , "border"
+                        , getPlanetBlurClass model HealthHousingEmployabilitySafety
+                        ]
+                    , onClick (FocusOrganisation SocialCareDirect)
+                    ]
+                    [ span [ class (getPlanetBlurClass model HealthHousingEmployabilitySafety) ]
+                        []
+                    ]
+                , li [ classes [ "saturn", getPlanetBlurClass model HealthHousingEmployabilitySafety ], onClick (FocusOrganisation REACHteam) ]
+                    [ span []
+                        [ span [ class "ring" ]
+                            [ text "" ]
+                        ]
+                    ]
+                , li [ classes [ "uranus", "border", getPlanetBlurClass model CulturePolitics ] ]
+                    [ a [ href "#uranus" ]
+                        [ span [ classes [ "1", getPlanetBlurClass model CulturePolitics ] ]
+                            []
+                        , span [ classes [ "2", getPlanetBlurClass model CulturePolitics ] ]
+                            []
+                        ]
+                    ]
+                , li [ classes [ "neptune", getPlanetBlurClass model CulturePolitics ] ]
+                    [ a [ href "#neptune" ]
+                        [ span []
+                            []
+                        ]
+                    ]
+                , li [ classes [ "pluto", getPlanetBlurClass model CulturePolitics ] ]
+                    [ a [ href "#pluto" ]
+                        [ span []
+                            []
+                        ]
+                    ]
                 ]
+            , footer [ class "footer dib" ]
+                (List.map viewFooterButton
+                    [ ( "Peers & Family", PeersFamily )
+                    , ( "School, Community & Neighbourhood", SchoolsCommunity )
+                    , ( "health, housing, employability & safety", HealthHousingEmployabilitySafety )
+                    , ( "culture and politics", CulturePolitics )
+                    ]
+                )
             ]
-        , footer [ class "footer dib" ]
-            (List.map viewFooterButton
-                [ ( "Peers & Family", PeersFamily )
-                , ( "School, Community & Neighbourhood", SchoolsCommunity )
-                , ( "health, housing, employability & safety", HealthHousingEmployabilitySafety )
-                , ( "culture and politics", CulturePolitics )
-                ]
-            )
-        ]
 
 
 viewFooterButton : ( String, OrganisationGroup ) -> Html Msg
@@ -153,3 +134,24 @@ getPlanetBlurClass model orgGroup =
 classes : List String -> Attribute msg
 classes cxs =
     class <| String.join " " cxs
+
+
+getOrganisationTitleAndDescription : Maybe Organisation -> ( String, String )
+getOrganisationTitleAndDescription focusedOrganisation =
+    case focusedOrganisation of
+        Nothing ->
+            ( "", "" )
+
+        Just focusedOrganisation ->
+            case focusedOrganisation of
+                REACHteam ->
+                    ( "REACH team", " a multi-professional and multi-disciplinary team with expertise in safeguarding, mental health, parenting, education and learning, health and communication to jointly assess, plan and intervene with young people presenting with high risk/high vulnerability" )
+
+                ChildrenServices ->
+                    ( "Barnet Children and Young People Plan", "A partnership with children, young people and different organisations that has produced a new child-friendly plan for 2016 - 2020 that reflects the priorities, needs and aspirations of the local population." )
+
+                CommunityPartners ->
+                    ( "Communities", "to be continued" )
+
+                SocialCareDirect ->
+                    ( "Social Care Direct", "For adults with social care needs and their carers, advice and information about adult social care services and safeguarding alerts" )
