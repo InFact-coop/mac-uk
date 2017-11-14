9fa37a97bba83608072a4fe16cfd18154f48763d
Max Gerber <mgerber94@gmail.com>
2017-11-14 11:47:53 +0000

    Made divs into links, links filling the whole screen currently #19

--- src/Routes/Home.elm
+++ src/Routes/Home.elm
@@ -9,7 +9,29 @@ import Types exposing (..)
 home : Model -> Html Msg
 home model =
     div [ class "w-60-ns center" ]
-        [ div
-            [ class "br-100 b--dark-grey w3 h3 bg-light-pink" ]
-            [ text "hello" ]
+        [ a [ href "#solar" class "w5" ]
+            [ div
+                [ class "br-100 b--dark-grey w5 f4 h5 bg-light-pink flex items-center justify-center" ]
+                [ text "Barnet REACH" ]
+            ]
+        , a [ href "#solar" class "w4" ]
+            [ div
+                [ class "br-100 b--dark-grey w4 f4 h4 bg-yellow flex items-center justify-center" ]
+                [ text "LB Havering" ]
+            ]
+        , a [ href "#solar" class "w4" ]
+            [ div
+                [ class "br-100 b--dark-grey w4 f4 h4 bg-light-green flex items-center justify-center" ]
+                [ text "St. Christopher's" ]
+            ]
+        , a [ href "#solar" class "w5" ]
+            [ div
+                [ class "br-100 b--dark-grey w5 f4 h5 bg-light-blue flex items-center justify-center" ]
+                [ text "10/10 Islington" ]
+            ]
+        , a [ href "#solar" class "w5" ]
+            [ div
+                [ class "br-100 b--dark-grey w5 f4 h5 bg-light-yellow flex items-center justify-center" ]
+                [ text "Future Haringey" ]
+            ]
         ]
